e219178bb934c6b6165a232f517ff9018689c6c8
Tyler Renelle <tylerrenelle@gmail.com>
2018-01-09 12:02:03 -0800

    Expose {Dropdown,Option,Spinner} from index.js so can create custom components. Add dist/* to git, since npm registry build steps needed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 /* @flow */
 
 import Editor from './Editor';
+import Dropdown from './components/Dropdown';
+import Option from './components/Option';
+import Spinner from './components/Spinner';
 
 module.exports = {
   Editor,
+  Dropdown,
+  Option,
+  Spinner
 };
